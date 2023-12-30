2db669cc80298864126df960511a362d38fc3237
Silas Huereca <silashuereca.dev@gmail.com>
2023-12-29 23:25:18 -0600

    fix text color in dark mode

--- components/App/ProjectCard.vue
+++ components/App/ProjectCard.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <h3 class="text-sm font-medium" v-text="project.name" />
-    <p class="text-sm text-gray-600 w-full" v-text="project.description" />
+    <p class="text-sm text-gray-600 w-full dark:text-gray-400" v-text="project.description" />
   </div>
 </template>
 
